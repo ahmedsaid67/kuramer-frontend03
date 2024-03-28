f599250d5fad52073afe30d3ecc56262f360a108
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2024-03-28 08:48:11 +0300

    Update KitapSerileri.js

--- compenent/KitapSerileri.js
+++ compenent/KitapSerileri.js
@@ -1,125 +1,122 @@
-// pages/kitapserileri.js
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import styles from '../styles/KitapSerileri.module.css';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-
 import Slider from 'react-slick';
 import { API_ROUTES } from '../utils/constants';
-import Image from 'next/image';
+import styles from '../styles/KitapSerileri.module.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'; 
 import Link from 'next/link';
-import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'; 
 
+// Helper function to convert titles to URL-friendly strings
 const convertToUrlFriendly = (text) => {
   if (text && typeof text === 'string') {
     const turkishCharacters = { 'ç': 'c', 'ğ': 'g', 'ı': 'i', 'ö': 'o', 'ş': 's', 'ü': 'u' };
     const cleanedText = text.trim().toLowerCase();
-    const urlFriendlyText = Array.from(cleanedText).map(char => turkishCharacters[char] || char).join('');
-    return urlFriendlyText.replace(/\s+/g, '-');
+    return Array.from(cleanedText).map(char => turkishCharacters[char] || char).join('').replace(/\s+/g, '-');
   }
   return '';
 };
 
-const CustomPrevArrow = (props) => {
-  const { onClick } = props;
-  return (
+const CustomPrevArrow = ({ onClick, imagesLoaded }) => (
+  imagesLoaded ? (
     <div className={styles.customPrevArrow} onClick={onClick}>
       <FaArrowLeft />
     </div>
-  );
-};
+  ) : null
+);
 
-const CustomNextArrow = (props) => {
-  const { onClick } = props;
-  return (
+const CustomNextArrow = ({ onClick, imagesLoaded }) => (
+  imagesLoaded ? (
     <div className={styles.customNextArrow} onClick={onClick}>
-       <FaArrowRight />
+      <FaArrowRight />
     </div>
-  );
-};
+  ) : null
+);
 
 export default function KitapSerileri() {
   const [kitapSerileri, setKitapSerileri] = useState([]);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   useEffect(() => {
-    // API çağrısı için kullanılan endpoint
-    const apiUrl = API_ROUTES.KITAP_KATEGORI_ACTIVE;
-
     const fetchData = async () => {
       try {
-        const response = await axios.get(apiUrl);
+        const response = await axios.get(API_ROUTES.KITAP_KATEGORI_ACTIVE);
         setKitapSerileri(response.data);
+        await preloadImages(response.data); // Preload images
       } catch (error) {
-        console.error('API çağrısı sırasında bir hata oluştu:', error.message);
+        console.error('API call error:', error.message);
       }
     };
 
-    fetchData(); // useEffect içinde API çağrısını başlat
+    fetchData();
+  }, []);
 
-  }, []); // Boş dependency array, sadece bir kere çağrılmasını sağlar
+  const preloadImages = async (kitapSerileri) => {
+    const promises = kitapSerileri.map((seri) =>
+      new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = seri.kapak_fotografi;
+        img.onload = resolve;
+        img.onerror = reject;
+      })
+    );
+    await Promise.all(promises);
+    setImagesLoaded(true); // Set imagesLoaded to true once all images are loaded
+  };
 
   const settings = {
     dots: true,
-    infinite: kitapSerileri.length >= 5,
+    infinite: kitapSerileri.length > 3,
     speed: 500,
-    slidesToShow: 5, // Gösterilecek kart sayısı
-    slidesToScroll: 1, // Kaydırılacak kart sayısı
-    prevArrow: <CustomPrevArrow />, // Özel önceki ok bileşeni
-    nextArrow: <CustomNextArrow /> ,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow imagesLoaded={imagesLoaded} />,
+    nextArrow: <CustomNextArrow imagesLoaded={imagesLoaded} />,
     responsive: [
-
       {
-        breakpoint: 1400, 
-        settings: {
-          slidesToShow: 3, 
-          infinite: kitapSerileri.length >= 3,
-        }
+        breakpoint: 1400,
+        settings: { slidesToShow: 3, infinite: kitapSerileri.length > 2 }
       },
       {
         breakpoint: 900,
-        settings: {
-          slidesToShow: 2, 
-          infinite: kitapSerileri.length >= 2,
-        }
+        settings: { slidesToShow: 2, infinite: kitapSerileri.length > 1 }
       },
       {
-        breakpoint: 768, // Ekran genişliği 768 pikselden küçükse
-        settings: {
-          slidesToShow: 1, // Gösterilecek kart sayısını 2'ye düşür
-          infinite: kitapSerileri.length >= 1,
-        }
+        breakpoint: 768,
+        settings: { slidesToShow: 1, infinite: kitapSerileri.length > 0 }
       }
     ]
   };
 
   return (
-    <>
-    {kitapSerileri.length > 0 && (
-        <div className={styles.container}>
-          <Link href={"/yayinlar/kitaplar"}>
-            <h1 className={styles.title}>Kitap Serileri</h1>
-          </Link>
-            <div className={styles.carouselContainer}>
-              <Slider {...settings}>
-                {kitapSerileri.map((seri) => (
-                  <div key={seri.id} className={styles.card}>
-                    <Link href={`/yayinlar/kitaplar?tab=${convertToUrlFriendly(seri.baslik)}`}>
-                      <img src={seri.kapak_fotografi} alt={seri.baslik} width={240} height={352} className={styles.cardImage} loading="eager" />
-                    </Link>
-                    <Link href={`/yayinlar/kitaplar?tab=${convertToUrlFriendly(seri.baslik)}`}>
-                      <p className={styles.cardText}>{seri.baslik}</p>
-                    </Link>
+    <div className={styles.container}>
+    {kitapSerileri.length === 0 ? (
+      // Display placeholder if there are no book series
+      <div className={styles.placeholder}></div>
+    ) : (
+      // Render the book series slider if data is available
+      <>
+        <Link href="/yayinlar/kitaplar">
+          <div><h1 className={styles.title}>Kitap Serileri</h1></div>
+        </Link>
+        <div className={styles.carouselContainer}>
+          <Slider {...settings}>
+            {kitapSerileri.map((seri) => (
+              <div key={seri.id} className={styles.card}>
+                <Link href={`/yayinlar/kitaplar?tab=${convertToUrlFriendly(seri.baslik)}`}>
+                  <div>
+                    <img src={seri.kapak_fotografi} alt={seri.baslik} width={240} height={352} className={styles.cardImage} />
+                    <p className={styles.cardText}>{seri.baslik}</p>
                   </div>
-                ))}
-              </Slider>
-          </div>
+                </Link>
+              </div>
+            ))}
+          </Slider>
         </div>
+      </>
     )}
-  </>
+  </div>
   );
 }
+
